3b9a8c02d7c02f883468d17d169f07eb78505432
Nirmalya Patra <101016078+nirmalya9@users.noreply.github.com>
2025-03-03 19:17:07 +0530

    Time: 0 ms (100%), Space: 17.9 MB (71.9%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> cnt;
-        using pii = pair<int, int>;
-        for (int x : nums) {
-            ++cnt[x];
-        }
-        priority_queue<pii, vector<pii>, greater<pii>> pq;
-        for (auto& [x, c] : cnt) {
-            pq.push({c, x});
-            if (pq.size() > k) {
-                pq.pop();
+        unordered_map<int, int> count;
+        for (const int num : nums)
+            ++count[num];
+
+        // Define a struct for priority queue
+        struct Compare {
+            bool operator()(const pair<int, int>& a, const pair<int, int>& b) {
+                return a.second > b.second; // Min-Heap based on frequency
             }
+        };
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, Compare> minHeap;
+
+        for (const auto& [num, freq] : count) {
+            minHeap.emplace(num, freq);
+            if (minHeap.size() > k)
+                minHeap.pop();
         }
+
         vector<int> ans;
-        while (!pq.empty()) {
-            ans.push_back(pq.top().second);
-            pq.pop();
+        while (!minHeap.empty()) {
+            ans.push_back(minHeap.top().first);
+            minHeap.pop();
         }
+
+        // Reverse to get the correct order
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
