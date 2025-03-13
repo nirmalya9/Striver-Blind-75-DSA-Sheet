9db243cf7fe5349e1d3b301a79517c60c6c50e73
Nirmalya Patra <101016078+nirmalya9@users.noreply.github.com>
2025-03-13 19:58:53 +0530

    Time: 0 ms (100%), Space: 8.5 MB (49.34%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+    // dp[i] := the number of ways to climb to the i-th stair
+    // Approach: 2D DP
+    vector<int> dp(n + 1);
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n; ++i)
+      dp[i] = dp[i - 1] + dp[i - 2];
+
+    return dp[n];
+    }
+};
